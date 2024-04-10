e236c4efc66b2d2882618e125fe35e0b2457d2c8
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-10 15:10:04 +0700

    write route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,9 +43,9 @@ class HomeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request)
     {
-        //
+        return view('welcome');
     }
 
     /**
