0af39d31238d1700286ff208c05da1158ecdcb89
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-16 11:55:23 +0700

    before

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    /**
+    * Create user
+    *
+    * @param  [string] name
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [string] password_confirmation
+    * @return [string] message
+    */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email'=>'required|string|unique:users',
+            'password'=>'required|string',
+            'c_password' => 'required|same:password'
+        ]);
+
+        $user = new User([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if($user->save()){
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+            'message' => 'Successfully created user!',
+            'accessToken'=> $token,
+            ],201);
+        }
+        else{
+            return response()->json(['error'=>'Provide proper details']);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:sanctum'], function() {
+      Route::get('logout', [AuthController::class, 'logout']);
+      Route::get('user', [AuthController::class, 'user']);
+    });
 });
+
+// Route::group(['prefix' => 'auth'], function () {
+//     Route::post('login', [AuthenticatedSessionController::class, 'login']);
+//     Route::post('register', [AuthenticatedSessionController::class, 'register']);
+
+//     Route::group(['middleware' => 'auth:sanctum'], function() {
+//       Route::get('logout', [AuthenticatedSessionController::class, 'logout']);
+//       Route::get('user', [AuthenticatedSessionController::class, 'user']);
+//     });
+// });
